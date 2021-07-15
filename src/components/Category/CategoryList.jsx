cda2db610ad722f8cd8467fb27c1c819f6647c85
Alissa Hodge <alissa.hodge@v75inc.com>
2021-07-15 09:37:51 -0400

    adding confirm dialog before removing a category

--- src/components/Category/CategoryList.jsx
+++ src/components/Category/CategoryList.jsx
@@ -7,11 +7,11 @@ import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell/TableCell";
 import TableBody from "@material-ui/core/TableBody";
 import Button from "@material-ui/core/Button";
-import DeleteIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from '@material-ui/icons/Edit';
 import TablePagination from "@material-ui/core/TablePagination";
 import {ExpenseTrackerContext} from "../../context/context";
-
+import ConfirmDialog from '../UI/ConfirmDialog/ConfirmDialog';
 
 const CategoryList = ({setCurrentId, setCategoryType, type, setFormData}) => {
     const classes = useStyles();
@@ -62,45 +62,52 @@ const CategoryList = ({setCurrentId, setCategoryType, type, setFormData}) => {
                 </TableHead>
                 <TableBody>{type === "Income" ?
 
-                        incomeCategories.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                            return (
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                                    {columns.map((column) => {
-                                        const value = row[column.id];
-                                        return (
-                                            <TableCell key={column.id} align={column.align}>
-                                                {column.id === 'actions' && <div>
-                                                    <Button
-                                                        onClick={() => handleRemoveCategory(row.id)}><DeleteIcon/></Button><Button
-                                                    onClick={() => handleUpdate(row)}><EditIcon/></Button>
-                                                </div>} {column.format && typeof value === 'number' ? column.format(value) : value}
-                                            </TableCell>
-                                        );
-                                    })}
-                                </TableRow>
-                            );
-                        })
-                     :
+                    incomeCategories.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                        return (
+                            <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                {columns.map((column) => {
+                                    const value = row[column.id];
+                                    return (
+                                        <TableCell key={column.id} align={column.align}>
+                                            {column.id === 'actions' && <div>
 
-                        expenseCategories.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                            return (
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                                    {columns.map((column) => {
-                                        const value = row[column.id];
-                                        return (
-                                            <TableCell key={column.id} align={column.align}>
-                                                {column.id === 'actions' && <div>
-                                                    <Button
-                                                        onClick={() => handleRemoveCategory(row.id)}><DeleteIcon/></Button><Button
-                                                    onClick={() => handleUpdate(row.id)}><EditIcon/></Button>
-                                                </div>} {column.format && typeof value === 'number' ? column.format(value) : value}
-                                            </TableCell>
-                                        );
-                                    })}
-                                </TableRow>
-                            );
-                        })
+                                                        <ConfirmDialog
+                                                            dialogText="Are You Sure You Want to Delete This?"
+                                                            okBtnText="Yes" cancelBtnTxt="No" openState={false}
+                                                            removeFunction={handleRemoveCategory} id={row.id} dialogBtn={<><Button><DeleteIcon/></Button></>}/>
 
+                                               <Button onClick={() => handleUpdate(row)}><EditIcon/></Button>
+                                            </div>} {column.format && typeof value === 'number' ? column.format(value) : value}
+                                        </TableCell>
+                                    );
+                                })}
+                            </TableRow>
+                        );
+                    })
+                    :
+
+                    expenseCategories.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                        return (
+                            <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                {columns.map((column) => {
+                                    const value = row[column.id];
+                                    return (
+                                        <TableCell key={column.id} align={column.align}>
+                                            {column.id === 'actions' && <div>
+
+                                                        <ConfirmDialog
+                                                            dialogText="Are You Sure You Want to Delete This?"
+                                                            okBtnText="Yes" cancelBtnTxt="No" openState={false}
+                                                            removeFunction={handleRemoveCategory} id={row.id} dialogBtn={<><Button><DeleteIcon/></Button></>}/>
+
+                                               <Button onClick={() => handleUpdate(row)}><EditIcon/></Button>
+                                            </div>} {column.format && typeof value === 'number' ? column.format(value) : value}
+                                        </TableCell>
+                                    );
+                                })}
+                            </TableRow>
+                        );
+                    })
 
 
                 }
